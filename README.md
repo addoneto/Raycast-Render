f84f07cb78a72970aa1fd665ebf3826f47f6383e
addo <addo.neto@gmail.com>
2021-01-24 15:37:12 -0300

    Player refactor

---
+++ README.md
@@ -0,0 +1,7 @@
+# Raycast Render
+
+Use casted rays with collision detection to simulate a three dimentional scene
+
+## Result
+
+## Explanation
--- script.js
+++ script.js
@@ -1,141 +1,170 @@
-let tileSize = 105, px, py, pVel = 5, angle = 45, fov = 1;
-let martix = [
-    [1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 1, 0, 0, 0, 1, 0, 1],
-    [1, 0, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 1, 0, 0, 0, 1],
-    [1, 0, 0, 1, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+const martix = [
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
-let rays = [];
+const tile = 100;
+let p;
 
 function start() {
-    createCanvas(martix[0].length * tileSize * 2, martix.length * tileSize);
+    createCanvas(martix[0].length * tile * 2, martix.length * tile);
 
-    px = martix[0].length * tileSize / 2;
-    py = martix.length * tileSize / 2 + tileSize / 2;
+    px = martix[0].length * tile / 2;
+    py = martix.length * tile / 2 + tile / 2;
 
-    for (let i = -(fov / 2); i < fov / 2; i++) {
-        rays.push(new Ray(px, py, i + angle));
-    }
+    p = new Player(
+        martix[0].length * tile / 2,
+        martix.length * tile / 2 + tile / 2,
+        0,
+        2,
+        70
+    );
 }
 
-function update() {
-    background("rgb(50,50,50)");
+function update(){
+    background("#232020");
+
     drawBoard();
-    drawPlayer();
-    castRays();
+    p.update();
 }
 
-function castRays() {
-    for (let i = 0; i < fov; i++) {
-        rays[i].update(px, py, i - fov / 2 + angle);
-        rays[i].cast();
-    }
-}
+function drawBoard(){
+    stroke(5, 'gray');
+    line(martix[0].length * tile, 0, martix[0].length * tile, martix.length * tile);
 
-function drawBoard() {
-    stroke(6, 'gray');
-    line(martix[0].length * tileSize, 0, martix[0].length * tileSize, martix.length * tileSize);
-
-    stroke(2, 'gray');
+    stroke(2, '#5a5858');
     for (let rows = 0; rows < martix.length; rows++) {
         for (let columns = 0; columns < martix[0].length; columns++) {
-            line(0, rows * tileSize, martix[0].length * tileSize, rows * tileSize);
-            line(columns * tileSize, 0, columns * tileSize, martix.length * tileSize);
+            line(0, rows * tile, martix[0].length * tile, rows * tile);
+            line(columns * tile, 0, columns * tile, martix.length * tile);
 
             if (martix[rows][columns] === 1) {
-                fill('black');
-                rect(columns * tileSize, rows * tileSize, tileSize, tileSize);
+                fill('#434343');
+                rect(columns * tile, rows * tile, tile, tile);
             }
         }
     }
 }
 
-function drawPlayer() {
-    noStroke();
-    fill('#ed174b');
-    circle(px, py, 10);
+function Player(_x, _y, _a, _v, _f) {
+    this.pos = new Vector2(_x, _y);
+    this.angle = _a;
+    this.vel = _v;
+    this.fov = _f;
+    this.dir = new Vector2(0, 0);
+
+    this.rays = [];
+
+    for (let i = -(this.fov / 2); i < this.fov / 2; i++) {
+        this.rays.push(new Ray(this.pos.x, this.pos.y, i + this.angle));
+    }
+
+    this.changeAngle = function(x){
+        this.angle += x;
+        if(this.angle > 360) this.angle = 1;
+        if(this.angle < 0) this.angle = 359;
+    }
+
+    this.setDir = function (_xD, _yD) {
+        if (_xD != null) this.dir.x = _xD;
+        if (_yD != null) this.dir.y = _yD;
+
+        //this.dir.Normalize();
+        const mag = Math.sqrt(this.dir.x ** 2 + this.dir.y ** 2);
+        if(mag === 0) return;
+        
+        this.dir.x /= mag;
+        this.dir.y /= mag;
+    }
+
+    this.update = function () {
+        this.castRays();
+
+        this.pos.x += this.dir.x * this.vel;
+        this.pos.y += this.dir.y * this.vel;
+
+        this.draw();
+    }
+
+    this.draw = function () {
+        noStroke();
+        fill('#ddc9b4');
+        circle(this.pos.x, this.pos.y, 10);
+    }
+
+    this.castRays = function(){
+        for (let i = 0; i < this.fov; i++) {
+            this.rays[i].update(this.pos.x, this.pos.y, i - this.fov / 2 + this.angle);
+            this.rays[i].cast();
+        }
+    }
+}
+
+function Ray(_x, _y, _angle) {
+    this.pos = new Vector2(_x, _y);
+    this.angle = _angle;
+    this.dir = Vector2.angleToDirection(_angle);
+
+    this.update = function (_x, _y, _angle) {
+        this.pos.x = _x;
+        this.pos.y = _y;
+        this.angle = _angle;
+        this.dir = Vector2.angleToDirection(this.angle);
+    }
+
+    this.draw = function (finalX, finalY) {
+        stroke(3, '#d75b5b'); line(this.pos.x, this.pos.y, finalX, finalY);
+    }
+
+    this.cast = function () {
+        this.draw(this.pos.x + this.dir.x * canvas.width,
+            this.pos.y + this.dir.y * canvas.width)
+    }
 }
 
 function keyDown(key) {
     switch (key) {
         case ARROW_RIGHT: // D
-            px += pVel;
+            p.setDir(1, null);
             break;
         case ARROW_LEFT: // A
-            px -= pVel;
+            p.setDir(-1, null);
             break;
         case ARROW_UP: // W
-            py -= pVel;
+            p.setDir(null, -1);
             break;
         case ARROW_DOWN: // S
-            py += pVel;
+            p.setDir(null, 1);
             break;
-        case 65: //A
-            angle += 1;
-            if (angle > 360) angle = 1;
+
+        case 65: // A
+            p.changeAngle(1);
             break;
         case 68: //D
-            angle -= 1;
-            if (angle < 0) angle = 359;
+            p.changeAngle(-1);
             break;
     }
 }
 
-class Ray {
-    constructor(_x, _y, _angle) {
-        this.position = new Vector2(_x, _y);
-        this.angle = _angle;
-        this.direction = Vector2.angleToDirection(_angle);
-    }
-
-    update(_x, _y, _angle) {
-        this.position.x = _x;
-        this.position.y = _y;
-        this.angle = _angle;
-        this.direction = Vector2.angleToDirection(this.angle);
-    }
-
-    draw(finalX, finalY) {
-        stroke(3, 'red');
-        line(this.position.x, this.position.y,
-            finalX, finalY);
-    }
-
-    cast() {
-        // HORIZONTAL LINES
-
-        let yPoint
-
-        if (this.angle < 180) yPoint = Math.floor(this.position.y / tileSize) * tileSize; // LOOKING UP
-        else yPoint = Math.ceil(this.position.y / tileSize) * tileSize; 
-
-        noStroke(); fill('purple'); circle(this.position.x, yPoint, 5);
-
-        let h = yPoint - this.position.y;
-
-        noFill(); stroke(2, 'gray');
-        circle(this.position.x, this.position.y, Math.abs(h));
-        noStroke();
-
-        let tangent = -1 / tan(angle) * h;
-
-        fill('blue');
-        circle(tangent + this.position.x, this.position.y, 5);
-
-        // why ? ? ? ? ?
-        //let inverseTangent = 1 / tan(angle);
-        //let xPoint = h * inverseTangent;
-
-        // xPoint is pased on the [0, 0] point, so to make it relative to the player position we should add its x pos
-        //xPoint += this.position.x;
-
-        //fill('blue'); circle(xPoint, yPoint, 5);
-
-        return this.draw(this.position.x + this.direction.x * canvas.width, this.position.y + this.direction.y * canvas.width);
+function keyUp(key) {
+    switch (key) {
+        case ARROW_RIGHT: // D
+            p.setDir(0, null);
+            break;
+        case ARROW_LEFT: // A
+            p.setDir(0, null);
+            break;
+        case ARROW_UP: // W
+            p.setDir(null, 0);
+            break;
+        case ARROW_DOWN: // S
+            p.setDir(null, 0);
+            break;
     }
 }
